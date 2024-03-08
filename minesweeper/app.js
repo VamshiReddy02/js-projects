6770ccf77bbcfc9140d8d0f001435fab8e9138a2
Vamshi Reddy <vamshiproject02@gmail.com>
2024-03-08 18:38:39 +0530

    project almost done
    Signed-off-by: Vamshi Reddy <vamshiproject02@gmail.com>

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -61,6 +61,68 @@ document.addEventListener('DOMContentLoaded', () =>{
     if(square.classList.contains('bomb')){
         gameOver()
     }
+    else {
+        let total = square.getAttribute('data')
+        if (total != 0) {
+            square.classList.add('checked')
+            if (total == 1) square.classList.add('one')
+            if (total == 2) square.classList.add('two')
+            if (total == 3) square.classList.add('three')
+            if (total == 4) square.classList.add('four')
+            square.innerHTML = total
+            return
+        }
+        checkSquare(square)
+    }
+    square.classList.add('checked')
+   }
+
+   function checkSquare(square) {
+    const currentId = square.id 
+    const isLeftEdge = (currentId.id % width === 0)
+    const isRightEdge = (currentId % width === width - 1)
+    setTimeout(function () {
+        if ( currentId > 0 && !isLeftEdge){
+        const newId = squares[parseInt(currentId) - 1].id
+        const newSquare = document.getElementById(newId)
+        click(newSquare)
+        }
+        if ( currentId > 9 && !isRightEdge){
+            const newId = squares[parseInt(currentId) - 1 - width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+            }
+        if ( currentId > 10){
+            const newId = squares[parseInt(currentId) - width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+            }
+        if ( currentId > 11 && !isLeftEdge){
+            const newId = squares[parseInt(currentId) - 1 - width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+            }
+        if ( currentId < 98 && !isRightEdge){
+            const newId = squares[parseInt(currentId) + 1].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+            }
+        if ( currentId > 90 && !isLeftEdge){
+            const newId = squares[parseInt(currentId) - 1 + width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+            }
+        if ( currentId < 88 && !isRightEdge){
+            const newId = squares[parseInt(currentId) + 1 + width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+            }
+        if ( currentId < 89 ){
+            const newId = squares[parseInt(currentId) + width].id
+            const newSquare = document.getElementById(newId)
+            click(newSquare)
+            }
+    }, 10)
    }
 
    function gameOver() {
