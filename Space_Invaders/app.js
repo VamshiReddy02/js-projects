2046349fb208c755bef9537cd9db2ac7d4295d60
Vamshi Reddy <vamshiproject02@gmail.com>
2024-03-16 23:07:22 +0530

    new project is added
    Signed-off-by: Vamshi Reddy <vamshiproject02@gmail.com>

--- Space_Invaders/app.js
+++ Space_Invaders/app.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () =>{
     const grid = document.querySelector('.grid')
+    const resultDisplay = document.querySelector('.results')
+    const width = 15
+    
+
+    for (let i = 0 ; i < width*width; i++){
+        const square = document.createElement('div')
+        grid.append(square)
+    }
 
 })
